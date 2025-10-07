0c1da06d82b4ca74b14324e784dccd6ea93916cb
Johan Barzallo <arielbarzallo-97@hotmail.com>
2025-10-07 16:57:04 -0500

    Update Spotify client ID in environment files for production and development configurations.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  spotifyClientId: '35c9a4b3c9db45268c463448bbd6fd25',
+  spotifyClientId: 'AQUI VA TU CLIENT ID',
   // Debe coincidir EXACTAMENTE con la URI registrada en el Dashboard de Spotify
   spotifyRedirectUri: 'https://jbarzallo97.github.io/spotify-jb7/callback',
   spotifyScopes: 'user-read-private user-read-email user-follow-read user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  spotifyClientId: '35c9a4b3c9db45268c463448bbd6fd25',
+  spotifyClientId: 'AQUI VA TU CLIENT ID',
   spotifyRedirectUri: 'http://localhost:4200/callback',
   spotifyScopes: 'user-read-private user-read-email user-follow-read user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative'
 };
